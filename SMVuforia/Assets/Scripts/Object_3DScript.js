bf8f0406f0c8872aaef37736cd6d97ce05a1803e
Carlos Henrique Stapait Junior <chsjwarrior@gmail.com>
2016-11-11 09:56:09 -0200

    nova coisas
    criei duas novas pasta:
    3D_Objects para colocar objetos 3d
    Script para colocar o scripts
    
    adcionei uma funcionalidade nova:
    o objeto 3d ficar rotacionando

---
+++ SMVuforia/Assets/Scripts/Object_3DScript.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+public var moveSpeed : float = 10f;
+public var turnSpeed : float = 50f;
+
+function Start () {
+
+}
+
+function Update () {
+transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+}
