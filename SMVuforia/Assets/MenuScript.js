cbc862274b73b1f8feafd9a44ad64e11a8f968cc
Carlos Henrique Stapait Junior <chsjwarrior@gmail.com>
2016-11-11 09:12:21 -0200

    smvuforia

---
+++ SMVuforia/Assets/MenuScript.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function MenuPrincipal() {
+    Application.LoadLevel("Menu");
+}
+
+function Jogar() {
+    Application.LoadLevel("scene");
+}
+
+function Sair() {
+    Application.Quit();
+}
