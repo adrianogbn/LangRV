bf93c7bc16a3eb9d20ff666a7cf3cf5c38265298
Carlos Henrique Stapait Junior <chsjwarrior@gmail.com>
2016-11-26 11:30:01 -0200

    update
    uptade

---
+++ SMVuforia/Assets/Scripts/Buttons.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Buttons : MonoBehaviour {
+    public Text textResult;
+
+	// Use this for initialization
+	void Start () {
+        textResult.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void click(string value) {
+        textResult.text.Insert(textResult.text.Length-1,value);
+    }   
+}
