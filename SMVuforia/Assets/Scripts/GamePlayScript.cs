c5fa3d1cbb6add70686c816b4fcf0706756f9139
Carlos Henrique Stapait Junior <chsjwarrior@gmail.com>
2016-11-27 19:37:26 -0200

    versão final parte 1
    segue a versão final do projeto.
    Falta apenas acrescentar sons e dificuldade.

--- SMVuforia/Assets/Scripts/GamePlayScript.cs
+++ SMVuforia/Assets/Scripts/GamePlayScript.cs
@@ -4,24 +4,36 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class GamePlayScript : MonoBehaviour {
-    private float timeLeft;    
-    public Button[] buttons = new Button[26];
+    private float timeLeft;
     public Text textTimer;
     public Text textResult;
 
+    public int objSize = 3;
+    public GameObject[] objects = new GameObject[3];
+    public string[] results = new string[3];
+
+    private int currentObj;
+    
     // Use this for initialization
     void Start () {
-        timeLeft = 120;
-        textResult.text = "";
+        for (int i = 0; i < objSize; i++)
+            results[i] = results[i].ToUpper();
+        sorteiaObject();
     }
 	
 	// Update is called once per frame
 	void Update () {
         timeLeft -= Time.deltaTime;
         textTimer.text = timeLeft.ToString();
-        
-        if (timeLeft < 0)
-            MenuPrincipal(); 
+
+        if (timeLeft >= 0) {
+            if (textResult.text.Length <= results[currentObj].Length) {
+                if (string.Equals(results[currentObj], textResult.text))
+                    sorteiaObject();
+            } else
+                textResult.text = "";
+        } else
+            MenuPrincipal();
     }
 
     public void ButtonsAction(string value) {
@@ -32,4 +44,12 @@ public class GamePlayScript : MonoBehaviour {
         SceneManager.LoadScene("Menu");
     }
 
+    private void sorteiaObject() {
+        for (int i = 0; i < objSize; i++)
+            objects[i].SetActive(false);
+        currentObj = Random.Range(0, 2);
+        objects[currentObj].SetActive(true);
+        timeLeft = 60;
+        textResult.text = "";
+    }
 }
